0b28c7399c3c3bc1386052a754230a5ee4cd23fe
Phil <ps12e@my.fsu.edu>
2019-03-05 18:08:15 -0500

    fixed iteration

--- network.h
+++ network.h
@@ -189,7 +189,7 @@ void Network::iterate(void)
 		*(newstate+i)=0;
 		for (int j=0;j<n;j++)
 		{
-			*(newstate+i)=(double)*(adjacency+i*n+j) * *(state+j);
+			*(newstate+i)+=(double)*(adjacency+i*n+j) * *(state+j);
 			sumelts+=(double)*(adjacency+i*n+j);
 		}
 		*(newstate+i) = *(newstate+i)/sumelts;
@@ -210,7 +210,7 @@ void Network::iterate(int k)
 			*(newstate+i)=0;
 			for (int j=0;j<n;j++)
 			{
-				*(newstate+i)=(double)*(adjacency+i*n+j) * *(state+j);
+				*(newstate+i)+=(double)*(adjacency+i*n+j) * *(state+j);
 				sumelts+=(double)*(adjacency+i*n+j);
 			}
 			*(newstate+i) = *(newstate+i)/sumelts;
