98f5cf3ab1257e445340aaada63ba928f606771c
Phil <ps12e@my.fsu.edu>
2019-03-05 18:28:33 -0500

    implemented iterate

--- Makefile
+++ Makefile
@@ -1,2 +1,3 @@
 all:
 	g++ -std=c++11 -o scalefree scalefree.cpp network.h;
+	g++ -std=c++11 -o scalefree_iterate scalefree_iterate.cpp network.h;
--- scalefree_iterate.cpp
+++ scalefree_iterate.cpp
@@ -3,9 +3,14 @@
 
 int main()
 {
+	int n = 10;
+	int iterations=0;
+	int converged;
 	Network network;
-	network.generate_scalefree(10,3,2);
-	network.genstate_normal(10,500);
+	network.generate_scalefree(n,3,2);
+	network.genstate_normal(0,1);
+	
+	double *oldstate = new double[n];
 	
 	std::cout << "The current state vector is:\n";
 	network.print_state();
@@ -13,7 +18,17 @@ int main()
 	std::cout << "\nNetwork adjacency is:\n";
 	network.print_adjacency();
 
-	network.iterate();
-	std::cout << "\nAfter 1 iterations, state is:\n";
+	do {
+		converged = 1;
+		for (int i=0;i<n;i++) { *(oldstate+i) = network.state[i]; } 
+		network.iterate();
+		for (int i=0;i<n;i++)
+		{
+			if ((float)network.state[i]!=(float)oldstate[i]) { converged = 0; }
+		}
+		iterations+=1;
+	} while (iterations<=100000&&converged==0);
+	
+	std::cout << "\nAfter " << iterations-1 << " iterations, state is:\n";
 	network.print_state();
 }
