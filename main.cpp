cfc41c3468de3cff2734bb09191f0329eb2e801c
Michael St. Denis <michaelstdenis7@gmail.com>
2023-08-22 19:43:20 -0400

    Testing linked list structure in main with user defined objects.

--- main.cpp
+++ main.cpp
@@ -22,8 +22,16 @@ private:
 int main(int argc, char** argv)
 {
 
+    Animal* a = new Animal("Fluffy");
+    Animal* b = new Animal("Scruffy");
+    Animal* c = new Animal("Moe");
 
-/*
+    LinkedList<Animal*>* aList = new LinkedList<Animal*>(0);
+
+    aList->add_first(a);
+    aList->add_last(c);
+    aList->add_at(1,b);
+
     LinkedList<int>* list = new LinkedList<int>(sizeof (int));
 
     list->add_first(0);
@@ -49,7 +57,7 @@ int main(int argc, char** argv)
         int element = list->remove(i);
         printf("%d\n", element);
     }
-*/
+
 
 
 	// Instantiating a DirectedGraph struct.
