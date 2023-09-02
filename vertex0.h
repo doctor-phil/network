28ed610f5903152c9da133116ba295ba770c13fc
Michael St. Denis <michaelstdenis7@gmail.com>
2023-09-02 14:08:57 -0400

    Updating implementations to reflect map and not LinkedList

---
+++ vertex0.h
@@ -0,0 +1,153 @@
+#pragma once
+#include <map>
+
+template<typename T, typename K> class Vertex {
+    public: 
+		Vertex(T data){
+			this->data     = data;
+            this->visited  = false;
+			this->distance = 0;
+			this->parent   = nullptr
+			this->arcMap   = new std::map<Vertex*, K>();
+			;
+		}
+
+		/*
+		 * This function adds an arc struct going from the Vertex* origin parameter
+		 * to the Vertex* dest parameter with a weight of the float price parameter.   
+		 */
+		bool addArc(Vertex* destination, K weight) {
+			if(destination == nullptr)
+				return false;
+
+			Vertex* arc = new Vertex(weight);
+			int prevSize = this->arcMap->size();
+	
+			this->arcMap->pair_insert(arc, weight);	
+
+			if(this->arcMap->size() - prevSize == 1) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		bool removeArc(Vertex* destination){
+			if(destination == nullptr)
+				return false;
+			
+		/*	for(int i = 0; i < prevSize; i++) {
+				Arc* arc = this->arcList->get(i);
+
+				if(arc->vertex->data == destination->data) {
+					this->arcList->remove(i);
+					break;
+				}
+			}
+            */
+           this->arcMap::iterator it;
+           it = this->arcMap->find(destination);
+           if(it != this->arcMap->end()){
+                this->arcMap->erase(destination);
+                return true;
+           } else {
+            return false;
+           }
+		}
+
+		T getData() {
+			return this->data;
+		}
+
+		std::map<Vertex*, K> get_arc_list() {
+			return this->arcMap;
+		}
+
+		bool getVisited() {
+			return this->visited;
+		}
+
+		void setVisited(bool visit) {
+			this->visited = visit;
+		}
+
+		/*
+		 * The get_weight function takes a Vertex* origin vertex that goes to the 
+		 * Vertex* destination vertex parameter and returns the weight of that arc 
+		 * if such an arc exists. -1 is returned if no such arc exists.
+		 */
+		K getWeight(Vertex* destination) {
+			if(destination == nullptr)
+				return -1;
+
+		    /*	
+            for(int i = 0; i < this->arcList->getSize(); i++) {
+			    	Arc* arc = this->arcList->get(i);
+					
+				// If the destination vertex is found in this vertex's arc list.	
+				// MAY NEED TO COMPARE THE VERTICES AND NOT THE DATA BASED ON MEMORY ADDRESS	
+				if(this->data == destination->data) {
+					return arc->weight;
+				}
+			}
+            */
+
+           this->arcMap::iterator it;
+           it = this->arcMap->find(destination);
+           if(it != this->arcMap->end()){
+                return this->arcMap[destination];
+           } else {
+            return -1;
+           }
+		}
+
+		/*
+		 * The change_vertex_weight function takes an origin and destination Vertex* and changes the
+		 * weight associated with the arc from the origin vertex to the destination vertex to the
+		 * value of the cost parameter. 
+		 */
+		bool changeWeight(Vertex* destination, K weight) {
+			if(destination == nullptr)
+				return false;
+
+			for(int i = 0; i < this->arcList->getSize(); i++) {
+				Arc* arc  = this->arcList->get(i);
+
+				// If this arc's vertex is equal to the destination vertex
+				// MAY NEED TO COMPARE THE VERTICES AND NOT THE DATA BASED ON MEMORY ADDRESS
+				if(destination->data == arc->vertex->data) {
+					arc->setWeight(weight);
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		void setDistance(K distance) {
+			this->distance = distance;
+		}	
+
+		K getDistance() {
+			return this->distance;
+		}
+			
+		bool hasArc(Vertex* v) {
+
+			}
+
+	private:
+		std::map<Vertex*, K> arcMap;
+		T data;
+		bool visited;
+		K distance;
+		Vertex* parent;
+
+        void setParent(Vertex* v) {
+			this->parent = v;
+		}
+
+		Vertex* getParent() {
+			return this->parent;
+		}
+};
